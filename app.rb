ac8a48db796e9913917eac0ca90905344d3166f8
Jonas Schneider <mail@jonasschneider.com>
2014-10-01 03:49:53 +0200

    add eyetrack awesomeness

--- app.rb
+++ app.rb
@@ -8,6 +8,8 @@ require 'geocoder/railtie'
 Geocoder::Railtie.insert
 require 'securerandom'
 
+require_relative 'constants'
+
 
 enable :sessions
 set :session_secret, ENV["SESSION_SECRET"]
@@ -32,8 +34,6 @@ class User < ActiveRecord::Base
   validates :uid, presence: true, uniqueness: true
 
   has_many :posts
-
-  attr_accessible :name, :address, :zip_city, :country, :phone, :email, :custom1_name, :custom1_value, :custom2_name, :custom2_value, :public_address
 
   before_validation :regenerate_token!, on: :create
 
@@ -84,11 +84,23 @@ class User < ActiveRecord::Base
   def city
     zip_city ? zip_city.gsub(/\d/, '').strip : nil
   end
+
+  def name
+    return "Raphaël Kraif" if uid == "kraifra"
+    super
+  end
+
+  def firstname
+    return "Nhung" if uid == "phantsa"
+    return "Raphaël" if uid == "kraifra"
+    len = I18n.transliterate(name).match(/^[A-Z][a-z]*(\-[A-Z][a-z]+)?/).try(:to_s).try(:length)
+    return unless len
+    name[0, len]
+  end
 end
 
 class Post < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :body
   validates_presence_of :body, :user
 end
 
@@ -109,6 +121,24 @@ helpers do
       string
     end
   end
+
+  def avatar(user, opts={})
+    scale = opts[:scale] || 2
+    id = SecureRandom.hex
+    source = "/users/#{user.uid}/avatar"
+
+    bg_color = "000"
+
+    markup = capture_haml do
+      haml_tag :svg, style: 'position: absolute;top:-100000px;left:-100000px' do
+        haml_tag :clipPath, id: id do
+          haml_tag :circle, cx: 30*scale, cy: 20*scale, r: 28*scale
+        end
+      end
+      haml_tag :img, alt: "Bild von #{user.name}", src: source, style: "width: #{58*scale}px; height: #{75*scale}px;margin-top:#{-5*scale}px; background: ##{bg_color}; -webkit-clip-path: circle(#{28*scale}px at #{30*scale}px #{30*scale}px);clip-path: url(##{id});-webkit-filter:saturate(0) brightness(1.4)"
+    end
+    markup
+  end
 end
 
 get '/' do
@@ -119,12 +149,18 @@ get '/all' do
   haml :all
 end
 
+if ENV["EYETRACK"]
+  get '/eyetrack' do
+    haml :eyetrack
+  end
+end
+
 get '/edit' do
   haml :edit
 end
 
 post '/update' do
-  if @user.update_attributes(params[:user])
+  if @user.update_attributes(params[:user].slice(:name, :address, :zip_city, :country, :phone, :email, :custom1_name, :custom1_value, :custom2_name, :custom2_value, :public_address))
     flash[:info] = 'Deine Kontaktdaten wurden gespeichert.'
     redirect '/'
   else
@@ -132,8 +168,14 @@ post '/update' do
   end
 end
 
+get "/users/:uid/avatar" do
+  u = User.where(uid: params[:uid]).first
+  content_type "image/jpg"
+  File.open("var/avatars/#{u.uid}.jpg")
+end
+
 post '/posts' do
-  post = @user.posts.build(params[:post])
+  post = @user.posts.build(params[:post].slice(:body))
   if post.save
     flash[:info] = 'Dein Eintrag wurde gespeichert.'
     redirect '/'
